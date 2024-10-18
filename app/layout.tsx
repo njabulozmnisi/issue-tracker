0c5c2eb340b85604410373631f998c13fdab5d94
Njabulo Mnisi <njabulozmnisi@gmail.com>
2024-10-19 01:24:03 +0200

    build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Theme>
             <NavBar/>
-            <main>
+            <main className="p-5">
                 {children}
             </main>
         </Theme>
