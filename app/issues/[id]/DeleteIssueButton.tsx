cc2a7194d979fa7d9ea6a3af5ad8a16a5685f1e4
Njabulo Mnisi <njabulozmnisi@gmail.com>
2024-10-19 15:15:10 +0200

    improve the user experience of deleting an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -4,17 +4,21 @@ import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 import axios from "axios";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
+import {Spinner} from "@/app/components";
 
 const DeleteIssueButton = ({issueId}: { issueId: number }) => {
     const router = useRouter();
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(false);
+    const [isDeleting, setDeleting] = useState(false);
 
     const deleteIssue = async () => {
         try {
+            setDeleting(true);
             await axios.delete('/api/issues/' + issueId);
             router.push('/issues');
             router.refresh();
         } catch (e) {
+            setDeleting(false);
             setError(true)
         }
     }
@@ -23,8 +27,9 @@ const DeleteIssueButton = ({issueId}: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color="red">
+                    <Button color="red" disabled={isDeleting}>
                         Delete Issue
+                        {isDeleting && <Spinner/>}
                     </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
