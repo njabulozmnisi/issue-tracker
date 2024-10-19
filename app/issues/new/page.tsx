f0de8b89fbe8ca8260c5f7ff81c93f1f7b3f8b34
Njabulo Mnisi <njabulozmnisi@gmail.com>
2024-10-19 14:38:25 +0200

    improve the loading exprience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import IssueForm from "@/app/issues/_components/IssueForm";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "@/app/issues/_components/issueFormSkeleton";
+
+const IssueForm = dynamic(
+    () => import("@/app/issues/_components/IssueForm"),
+    {
+        ssr: false,
+        loading: () => <IssueFormSkeleton />,
+    },
+)
 
 const NewIssuePage = () => {
     return (
-        <IssueForm />
+        <IssueForm/>
     );
 };
 
