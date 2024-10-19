5e2a10a0584df54e7df1090be53f78937d3ed11d
Njabulo Mnisi <njabulozmnisi@gmail.com>
2024-10-19 11:52:46 +0200

    refactoring app component imports

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import {Box} from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
+import {Skeleton} from "@/app/components";
 
 const LoadingNewIssuePage = () => {
     return (
         <Box className="max-w-xl">
             loading...
-            <Skeleton />
+            <Skeleton/>
             <Skeleton height="20rem"/>
         </Box>
     );
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {Table} from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import Link from "@/app/components/Link";
-import delay from "delay";
+import {IssueStatusBadge, Link} from "@/app/components";
 import IssueActions from "@/app/issues/IssueActions";
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany();
-    await delay(2000)
 
     return (
         <div>
